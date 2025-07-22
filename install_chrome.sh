4fe3938cdf9debca23dfd9fa18c552739d741b31
Tiago Andrade <tiagoh736@gmail.com>
2025-07-22 13:28:56 -0300

    add install_chrome.sh

--- install_chrome.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "🛠 Instalando Google Chrome no ambiente do Render..."
-
-apt-get update
-apt-get install -y wget gnupg unzip curl
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt-get install -y ./google-chrome-stable_current_amd64.deb
-
-echo "✅ Chrome instalado com sucesso."
